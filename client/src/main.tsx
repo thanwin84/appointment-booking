31e107aefded3fbc553267a871fe876cba2fed58
kmtusher97 <kmtusher97@gmail.com>
2025-02-07 22:46:14 +0600

    Configure client router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+
+import { BookSlotPage, HomePage } from './pages';
+
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/book-slot" element={<BookSlotPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ client/src/pages/BookSlotPage.tsx
@@ -0,0 +1,3 @@
+export const BookSlotPage = () => {
+  return <div>BookSlotPage</div>;
+};
