b5aec668ec7f3c87a07c13a63d10b23e49de7da7
kmtusher97 <kmtusher97@gmail.com>
2025-02-07 22:30:34 +0600

    Init basic server

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/_status', (req, res) => {
+  res.send('Test!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
