63e31165b48309705361ca4b854578fd55133213
Armstrong104 <ameertalha22@gmail.com>
2025-02-13 18:02:31 +0600

    Resolve alias issue

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,13 @@
-import path from 'path';
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src/'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
