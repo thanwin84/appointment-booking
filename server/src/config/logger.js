884f2a0d23641036ff6ae5e4625b7c95e5130b00
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-03-20 10:38:13 +0600

    format the logger.js file

--- server/src/config/logger.js
+++ server/src/config/logger.js
@@ -1,23 +1,25 @@
-import pino from 'pino'
+import pino from 'pino';
 const transport = pino.transport({
-    target: 'pino-pretty',
-    options: {
-         colorize: true,
-         singleLine: true,
-    }
-})
+  target: 'pino-pretty',
+  options: {
+    colorize: true,
+    singleLine: true,
+  },
+});
 
-export const logger = pino({
+export const logger = pino(
+  {
     level: 'info',
     redact: [
-        'req.headers.authorization', 
-        'req.body.password',        
-        'req.body.confirmPassword',
-        'req.body.phoneNumber', 
-        'res.headers["set-cookie"]', 
-        '*.creditCard',            
-        '*.ssn',                   
-      ],
-      timestamp: pino.stdTimeFunctions.isoTime,
-
-}, transport)
+      'req.headers.authorization',
+      'req.body.password',
+      'req.body.confirmPassword',
+      'req.body.phoneNumber',
+      'res.headers["set-cookie"]',
+      '*.creditCard',
+      '*.ssn',
+    ],
+    timestamp: pino.stdTimeFunctions.isoTime,
+  },
+  transport
+);
