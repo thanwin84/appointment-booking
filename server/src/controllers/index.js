0f1b365bddb8ae73bd055df4c070b57686668b93
nazma98 <nazmasarker1311@gmail.com>
2025-03-20 20:37:46 +0600

    Add slotController for CRUD operation

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,2 +1,3 @@
 export * as authController from './auth.js';
 export * as signupController from './signup.js';
+export * as slotController from './slot.js';
---
+++ server/src/controllers/slot.js
@@ -0,0 +1,25 @@
+import asyncHandler from 'express-async-handler';
+
+import { slotServices } from '../services/index.js';
+
+export const createSlot = asyncHandler(async (req, res) => {
+    const slotData = await slotServices.createSlot(req.body);
+    res.status(201).send(slotData);
+});
+
+export const getSlot = asyncHandler(async (req, res) => {
+    const slots = await slotServices.getSlot();
+    res.json(slots);
+});
+
+export const updateSlot = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const updatedSlot = await slotServices.updateSlot(id, req.body);
+    res.status(201).json(updatedSlot);
+});
+
+export const deleteSlot = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    await slotServices.deleteSlot(id);
+    res.status(204).end();
+});
