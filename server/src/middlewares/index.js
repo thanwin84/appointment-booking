0d234932c45e11aa2132a929f1ec06ab8d7dbfab
nazma98 <nazmasarker1311@gmail.com>
2025-03-20 20:38:43 +0600

    Add validatePayload middleware

--- server/src/middlewares/index.js
+++ server/src/middlewares/index.js
@@ -1,2 +1,3 @@
 export { default as errorHandler } from './errorHandler.js';
 export { default as authenticate } from './authenticate.js';
+export { default as validatePayload } from './validationMiddleware.js';
---
+++ server/src/middlewares/validationMiddleware.js
@@ -0,0 +1,9 @@
+const validatePayload = (schema) => {
+    return (req, res, next) => {
+        const payload = req.body;
+        schema.parse(payload);
+        next();
+    };
+};
+
+export default validatePayload;
