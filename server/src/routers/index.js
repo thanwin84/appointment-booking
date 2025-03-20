c9cb75aab9d272717421006fd340b5b19016f199
nazma98 <nazmasarker1311@gmail.com>
2025-03-20 20:41:21 +0600

    Add API routes

--- server/src/routers/index.js
+++ server/src/routers/index.js
@@ -2,6 +2,7 @@ import { authenticate } from '../middlewares/index.js';
 
 import authRouter from './auth.js';
 import signupRouter from './signup.js';
+import slotRouter from './slot.js';
 import userRouter from './userRoute.js';
 
 const configureRouter = (app) => {
@@ -12,6 +13,7 @@ const configureRouter = (app) => {
   app.use('/api/auth', authRouter);
   app.use('/api/signup', signupRouter);
   app.use('/api/users', authenticate, userRouter);
+  app.use('/api/slots', slotRouter);
 };
 
 export default configureRouter;
---
+++ server/src/routers/slot.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import { slotController } from '../controllers/index.js';
+import { validatePayload } from '../middlewares/index.js';
+import { slotSchema } from '../schemas/slot.js';
+
+
+const slotRouter = express.Router();
+
+slotRouter.post(
+    '/', 
+    validatePayload(slotSchema.omit({ _id: true })),
+    slotController.createSlot);
+slotRouter.get('/', slotController.getSlot);
+slotRouter.put(
+    '/:id',
+    validatePayload(slotSchema.partial()),
+    slotController.updateSlot);
+slotRouter.delete('/:id', slotController.deleteSlot);
+
+export default slotRouter;
