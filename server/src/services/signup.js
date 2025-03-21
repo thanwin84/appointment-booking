2d4478b7acb3fb240be7a36e6ed6fe718a0af36b
thanwin84 <thanwinhline84@gmail.com>
2025-03-22 03:52:29 +0600

    use DuplicateErrorResourceError class in signup

--- server/src/services/signup.js
+++ server/src/services/signup.js
@@ -1,6 +1,13 @@
 import { User } from '../models/index.js';
+import { DuplicateResourceError } from '../utils/customErrors.js';
 
 export const signup = async ({ name, email, mobile, password }) => {
+  const userAlredyExists = await User.findOne({ email });
+  if (userAlredyExists) {
+    throw new DuplicateResourceError(
+      `User with email ${email} is already registered`
+    );
+  }
   const user = new User({
     name,
     email,
